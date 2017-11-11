8891203ccdef0bc16ed13c6cee4edd2839932f3a
Michael Zhang <failed.down@gmail.com>
2017-11-11 03:22:05 -0600

    apparently it doesn't coredump anymore. hooray

--- src/playfield.c
+++ src/playfield.c
@@ -3,9 +3,7 @@
 #include "replay.h"
 
 void free_playfield(playfield_t *p) {
-    // TODO: dunno why this coredumps
-    // free_beatmap(p->beatmap);
-    free(p->beatmap);
+    free_beatmap(p->beatmap);
     free_replay(p->replay);
     free(p);
 }
